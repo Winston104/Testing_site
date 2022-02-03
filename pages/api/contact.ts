a792f87d08da198bf1d7e36b2dad9533011e4c21
Winston104 <54595899+Winston104@users.noreply.github.com>
2022-02-02 18:56:37 -0600

    Update contact.ts

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -3,15 +3,15 @@ import vCardsJS from 'vcards-js';
 
 const handler = (req: NextApiRequest, res: NextApiResponse): void => {
   const vCard = vCardsJS();
-  vCard.firstName = 'Michael';
-  vCard.lastName = 'Hall';
-  vCard.email = 'michael.hall17@icloud.com';
-  vCard.birthday = new Date(2000, 5, 12);
+  vCard.firstName = 'Levi';
+  vCard.lastName = 'Cheney';
+  vCard.email = 'levicheney3012@gmail.com';
+  vCard.birthday = new Date(11, 16, 2000);
   vCard.photo.attachFromUrl(
-    'https://michael-hall.me/static/images/profile.jpeg',
+    'https://scontent-ort2-1.xx.fbcdn.net/v/t39.30808-6/273146104_4869167276511232_2859202710331493039_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=LWG2dtYvOXkAX8tYLGg&_nc_ht=scontent-ort2-1.xx&oh=00_AT9uMZikiEc2iXHVnt456gh5BEzSnY_72jpvTtEK5P9xGg&oe=61FFCA16',
     'JPEG'
   );
-  vCard.socialUrls['github'] = 'https://github.com/mah51';
+  vCard.socialUrls['github'] = 'https://github.com/Winston104';
 
   res.setHeader('Content-Type', 'text/vcard; name=vcard.vcf');
   res.setHeader('Content-Disposition', 'inline; filename=vcard.vcf');
